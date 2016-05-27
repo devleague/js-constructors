dd9f9cbdec9cc83c0756f19834b056430774cabd
Tanachi <tanachige@gmail.com>
2016-05-26 18:31:25 -1000

    Added Spellcaster constructor

--- constructors.js
+++ constructors.js
@@ -74,7 +74,12 @@ DamageSpell.prototype = Object.create(Spell.prototype);
  * @method  spendMana
  * @method  invoke
  */
-
+function Spellcaster(name, health, mana){
+  this.name = name;
+  this.health = health;
+  this.mana = mana;
+  this.isAlive = true;
+}
   /**
    * @method inflictDamage
    *
