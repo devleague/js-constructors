5764732a29b7ab9526521a5b334e48e837661f0d
KanKan <cindykan@KanKan.local>
2018-01-13 22:19:17 -1000

    Up to DamageSpell completed

--- constructors.js
+++ constructors.js
@@ -11,6 +11,12 @@
  * @method   getDetails
  */
 
+ function Spell (name, cost, description) {
+   this.name = name;
+   this.cost = cost;
+   this.description = description;
+ }
+
   /**
    * Returns a string of all of the spell's details.
    * The format doesn't matter, as long as it contains the spell name, cost, and description.
@@ -18,6 +24,10 @@
    * @name getDetails
    * @return {string} details containing all of the spells information.
    */
+
+   Spell.prototype.getDetails = function () {
+     return this.name + ' ' + this.cost + ' ' + this.description;
+   }
 
 /**
  * A spell that deals damage.
@@ -44,6 +54,12 @@
  * @property {string} description
  */
 
+ function DamageSpell (name, cost, damage, description) {
+   Spell.call(this, name, cost, description)
+   this.damage = damage;
+ }
+ 
+ DamageSpell.prototype = Object.create(Spell.prototype);
 /**
  * Now that you've created some spells, let's create
  * `Spellcaster` objects that can use them!
